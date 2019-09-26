319864901d4770365e3e4a6fa48e044df8a63799
ASUS <ASUS@DESKTOP-6TMOBF9>
2019-09-26 19:28:58 +0700

    commit

---
+++ src/pbo2/pert1/pkg10118046/latihan15/logika/PBO2PERT110118046Latihan15Logika.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pbo2.pert1.pkg10118046.latihan15.logika;
+
+/**
+ *
+ * @author
+ *  Nama              : M. Hilmy Naufal
+ *  Kelas             : IF2
+ *  NIM               : 10118046
+ *  Deskripsi Program : menampilkan hasil dari logika logika yang digunakan
+ */
+public class PBO2PERT110118046Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        boolean a = true;
+        boolean b= false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+
+}
